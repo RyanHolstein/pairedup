61f0219ec5e4c4a3b337d4662de80f59e25e2782
Ryan Holstein <ryan.holstein@gmail.com>
2014-04-15 17:50:26 -0700

    precompiled assets locally

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Paired::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -58,8 +58,7 @@ Paired::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets.
-  # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif]
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
